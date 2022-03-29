6a720f46c2243967b4f6a2863277ae760f25395b
Itzme-Lucifer <96771803+Itzme-lucifer@users.noreply.github.com>
2022-03-29 08:35:05 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,20 +7,18 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://telegra.ph/file/f6ec3d3d845a1e3dcce39.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/7a89888972934afb097f3.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Alexa bot created by Turbo*
-*Creator number : wa.me/916380260672?text=Hi%20Turbo%20Mods.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Evania bot created by Akhil Mahesh*
+*Creator number : wa.me/919778062036?text=Hi%20Itzme%20Lucifer.%20*
 
-*Githublink :    https://github.com/TURBOHYPER/Toxic-Alexa*
+*Githublink :    https://github.com/Itzme-lucifer/Evania-1.2*
 
-*Audio commads :   https://github.com/TURBOHYPER/Toxic-Alexa/tree/master/uploads*
+*Audio commads :   https://github.com/Itzme-lucifer/Evania-1.2/tree/master/uploads*
 
 *Sticker commads : coming soon*
 
-*My channel link : https://youtube.com/c/TurboMods* 
-
-*For More Updates Subscribe The Channel ☝*
+*For More Updates Contact me☝*
 `}) 
 
 }));
